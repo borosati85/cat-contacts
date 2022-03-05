9e33525ce70d54379d7e27b665bd31615762b597
Attila Boros <borosati85@gmail.com>
2022-03-05 12:59:56 +0100

    Updates

---
+++ precache-manifest.bfc8596fa30a2e8ea006f8824183b934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a7f2af70333a767b2545a305a20c0c",
+    "url": "/cat-contacts/index.html"
+  },
+  {
+    "revision": "d5c1b71d08645317c474",
+    "url": "/cat-contacts/static/css/main.ce0660c0.chunk.css"
+  },
+  {
+    "revision": "c9efb6796686e7b58617",
+    "url": "/cat-contacts/static/js/2.ffae79b6.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/cat-contacts/static/js/2.ffae79b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5c1b71d08645317c474",
+    "url": "/cat-contacts/static/js/main.389abd9b.chunk.js"
+  },
+  {
+    "revision": "784333c1df690b06e62d",
+    "url": "/cat-contacts/static/js/runtime-main.aaa7db62.js"
+  }
+]);
